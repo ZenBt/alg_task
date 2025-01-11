72d2f6afa93997afa30ed25d62bb72d94cbdde95
Игорь Сагайдак <zenbee111@gmail.com>
2025-01-11 18:19:08 +0300

    fixes

--- src/com/heroes_task/programs/GeneratePresetImpl.java
+++ src/com/heroes_task/programs/GeneratePresetImpl.java
@@ -10,15 +10,25 @@ public class GeneratePresetImpl implements GeneratePreset {
 
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
+        System.out.println("start generating");
         // Создаем новый объект армии
         Army computerArmy = new Army();
         List<Unit> selectedUnits = new ArrayList<>();
         int currentPoints = 0;
 
+        // Размер игрового поля
+        final int WIDTH = 27;
+        final int HEIGHT = 21;
+
+        // Множество занятых координат
+        Set<String> occupiedCoordinates = new HashSet<>();
+
         // Сортируем юнитов по двум ключам: эффективность атаки и здоровья к стоимости
         unitList.sort(Comparator.comparingDouble(
-            unit -> -(unit.getBaseAttack() / (double) unit.getCost() + unit.getHealth() / (double) unit.getCost())
-        ));
+                unit -> -(unit.getBaseAttack() / (double) unit.getCost()
+                        + unit.getHealth() / (double) unit.getCost())));
+
+        Random random = new Random();
 
         // Идем по каждому типу юнита
         for (Unit unit : unitList) {
@@ -26,19 +36,33 @@ public class GeneratePresetImpl implements GeneratePreset {
 
             // Добавляем юнитов, пока это возможно
             while (unitCount < 11 && currentPoints + unit.getCost() <= maxPoints) {
-                // Клонируем юнита, чтобы добавлять уникальные экземпляры
+                // Генерация уникальной случайной координаты
+                int x, y;
+                String coordinateKey;
+                do {
+                    x = random.nextInt(WIDTH);
+                    y = random.nextInt(HEIGHT);
+                    coordinateKey = x + "_" + y; // Ключ для проверки уникальности
+                } while (occupiedCoordinates.contains(coordinateKey));
+
+                // Помечаем координату как занятую
+                occupiedCoordinates.add(coordinateKey);
+
+                // Клонируем юнита с новой уникальной координатой
                 Unit newUnit = new Unit(
-                    unit.getName(),
-                    unit.getUnitType(),
-                    unit.getHealth(),
-                    unit.getBaseAttack(),
-                    unit.getCost(),
-                    unit.getAttackType(),
-                    new HashMap<>(unit.getAttackBonuses()),
-                    new HashMap<>(unit.getDefenceBonuses()),
-                    unit.getxCoordinate(),
-                    unit.getyCoordinate()
-                );
+                        unit.getName(),
+                        unit.getUnitType() + " " + unitCount,
+                        unit.getHealth(),
+                        unit.getBaseAttack(),
+                        unit.getCost(),
+                        unit.getAttackType(),
+                        new HashMap<>(unit.getAttackBonuses()),
+                        new HashMap<>(unit.getDefenceBonuses()),
+                        x,
+                        y);
+
+                System.out.println(newUnit.getName() + " x:" + newUnit.getxCoordinate() + " y:" +
+                        newUnit.getyCoordinate());
                 selectedUnits.add(newUnit);
                 currentPoints += newUnit.getCost();
                 unitCount++;
@@ -48,7 +72,7 @@ public class GeneratePresetImpl implements GeneratePreset {
         // Устанавливаем список юнитов и очки в армию
         computerArmy.setUnits(selectedUnits);
         computerArmy.setPoints(currentPoints);
-
+        System.out.println("finish generating");
         return computerArmy;
     }
 }
